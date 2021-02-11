b97e43b282b10bf994075b1a8560f462db99b522
mallocLevy <berkercaner1@gmail.com>
2021-02-11 08:36:09 +0300

    main added

--- lesson3/frogjmp/frogjmp.c
+++ lesson3/frogjmp/frogjmp.c
@@ -8,3 +8,17 @@ int solution(int X, int Y, int D){
 	else
 		return w+1;
 }
+
+int main(){
+	int x,y,d;
+	while(1){
+		printf("\nEnter values:");
+		printf("\nX: ");
+		scanf("%d",&x);
+		printf("\nY: ");
+		scanf("%d",&y);
+		printf("\nD: ");
+		scanf("%d",&d);
+		printf("\nAnswer: %d",solution(x,y,d));
+	}
+}
