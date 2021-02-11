6fa760d8f1d84b21df2d0619581c8d74dad1e9d2
mallocLevy <berkercaner1@gmail.com>
2021-02-11 16:14:23 +0300

    comments added for permissingelem

--- lesson3/permMissingElem/permMissingElem.c
+++ lesson3/permMissingElem/permMissingElem.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int solution(int A[], int N){
-	long sum = (long)(N+1)*(long)(N+2)>>1;
+	long sum = (long)(N+1)*(long)(N+2)>>1; difference of sum of 1...N+1 and sum of array elements will give the result;
 	for(int i=0; i<N; i++)
 		sum -= (long)A[i];
 	return (int)sum;
