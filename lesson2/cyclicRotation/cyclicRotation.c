817a8551aa7ff0a40229e9d88d79990bb2321efb
mallocLevy <berkercaner1@gmail.com>
2021-02-11 16:06:47 +0300

    comments are added

--- lesson2/cyclicRotation/cyclicRotation.c
+++ lesson2/cyclicRotation/cyclicRotation.c
@@ -16,9 +16,9 @@ struct Results Solution(int A[], int N, int K){ // K means how many times will b
 	temp.N = N;
 	
 	for(counter = 0; counter < N; counter++){
-		minShift = K % N;
+		minShift = K % N;						// it's for reducing cycles of for loop
 		i = counter;
-		for(minShift; minShift > 0; minShift--){
+		for(minShift; minShift > 0; minShift--){ // finding the position of the element where will be shifted
 			if(i == N-1)
 				i = 0;
 			else
@@ -32,6 +32,9 @@ struct Results Solution(int A[], int N, int K){ // K means how many times will b
 
 int main(){
 	int A[] = {1,2,3,4};
+	printf("Base Array: ");
+	for(int i=0;i<4;i++)
+		printf("%d ",A[i]);
 	struct Results main = Solution(A,4,9);
 	for(int i=0;i<4;i++)
 		printf(" %d ",main.A[i]);
