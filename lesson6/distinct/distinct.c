87485fee19f6d1382ba8112383c67ab1f6fb5627
mallocLevy <berkercaner1@gmail.com>
2021-02-15 16:12:09 +0300

    distinct.c is added

---
+++ lesson6/distinct/distinct.c
@@ -0,0 +1,39 @@
+#define maxValue 1000000  // 1000000 is the max value of an element of the array A
+
+struct checkElement{
+	int* checkNeg;
+	int* checkPos;
+}checkElement;
+
+int solution(int A[], int N){
+	int negCount = 0, posCount = 0, isSame = A[0];
+	
+	for(int i=1; i<N; i++){
+		isSame ^= A[i];
+	}
+	if(isSame == 0)
+		return 0;
+	
+	checkElement.checkNeg = (int*) calloc(maxValue,sizeof(int));
+	checkElement.checkPos = (int*) calloc(maxValue,sizeof(int));
+	
+	for(int i=0; i<N; i++){
+		if(A[i] < 0){
+			if(checkElement.checkNeg[-A[i]] == 0)
+				checkElement.checkNeg[-A[i]] = 1;
+		}
+		else{
+			if(checkElement.checkPos[A[i]] == 0)
+				checkElement.checkPos[A[i]] = 1;
+		}
+	}	
+	for(int i=0; i<maxValue; i++){
+		if(checkElement.checkNeg[i] == 1)
+			negCount++;
+		if(checkElement.checkPos[i] == 1)
+			posCount++;
+	}
+	free(checkElement.checkPos);
+	free(checkElement.checkNeg);
+	return negCount+posCount;
+}
