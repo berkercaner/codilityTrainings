413ad6edf7c9d0928df5cbb6310e82050922da63
mallocLevy <berkercaner1@gmail.com>
2021-02-15 14:57:25 +0300

    passingCars.c is added

---
+++ lesson5/passingCars/passingCars.c
@@ -0,0 +1,32 @@
+int solution(int A[], int N){
+	int ones = 0, i = 0, j = 0;
+	int zeroCount = 0, preZeroCount =0, result = 0;
+	int* pass;
+	
+	for(i=0; i<N; i++){
+		if(A[i] == 1);
+			ones++;
+	}
+	pass = (int*) calloc(ones,sizeof(int));
+	
+	for(i=0; i<N; i++){
+		if(A[i] == 0)
+			zeroCount++;
+		else{
+			pass[j] = zeroCount + preZeroCount;
+			preZeroCount += zeroCount;
+			zeroCount = 0;
+			j++;
+		}
+	}
+	
+	for(i=0; i<ones; i++){
+		result += pass[i];
+		if(result > 1000000000){
+			free(pass);
+			return -1;
+		}
+	}
+	free(pass);
+	return result;
+}
