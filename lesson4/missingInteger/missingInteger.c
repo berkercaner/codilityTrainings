f875586aa3040d5d8ac43d01008bb479802863bb
mallocLevy <berkercaner1@gmail.com>
2021-02-15 09:03:15 +0300

    missinInteger.c is added

---
+++ lesson4/missingInteger/missingInteger.c
@@ -0,0 +1,26 @@
+int solution(int A[], int N){
+	int i, max = 0, j = 0;
+	
+	for(i=0; i<N; i++){
+		if(A[i] > max)
+			max = A[i];
+		if(A[i] < 0)
+			j++;
+	}
+	if(i == j)
+		return 1;
+	
+	int* checkA = (int*)calloc(max,sizeof(int));
+	for(i=0; i<N; i++){
+		if(A[i] > 0){
+			if(checkA[A[i]-1] == 0){
+				checkA[A[i]-1] = 1;
+			}
+		}
+	}
+	for(i=0; i<max; i++){
+		if(checkA[i] == 0)
+			return i+1;
+	}
+	return max+1;
+}
