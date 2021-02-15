ce6ab8169f6822af9382591789420b3582f564fa
mallocLevy <berkercaner1@gmail.com>
2021-02-15 17:42:28 +0300

    cyclicrotatin.c editted

--- lesson2/cyclicRotation/cyclicRotation.c
+++ lesson2/cyclicRotation/cyclicRotation.c
@@ -1,16 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct Results{
-	int* A;
-	int N; // length
-};
-
-struct Results Solution(int A[], int N, int K){ // K means how many times will be shifted
+struct Results solution(int A[], int N, int K){ // K means how many times will be shifted
 	struct Results temp;
-	int counter;
-	int i;
-	int minShift;
-	
+	int counter,i,minShift;
 	
 	temp.A = (int *) malloc(sizeof(int)*N);
 	temp.N = N;
@@ -18,7 +8,7 @@ struct Results Solution(int A[], int N, int K){ // K means how many times will b
 	for(counter = 0; counter < N; counter++){
 		minShift = K % N;						// it's for reducing cycles of for loop
 		i = counter;
-		for(minShift; minShift > 0; minShift--){ // finding the position of the element where will be shifted
+		for(int j=minShift; j>0; j--){ // finding the position of the element where will be shifted
 			if(i == N-1)
 				i = 0;
 			else
@@ -28,16 +18,4 @@ struct Results Solution(int A[], int N, int K){ // K means how many times will b
 	}
 	
 	return temp;
-}
-
-int main(){
-	int A[] = {1,2,3,4};
-	printf("Base Array: ");
-	for(int i=0;i<4;i++)
-		printf("%d ",A[i]);
-	struct Results main = Solution(A,4,9);
-	for(int i=0;i<4;i++)
-		printf(" %d ",main.A[i]);
-		
-	return 0;
 }
