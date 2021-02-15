d942142591e24c298b86f301533f215d66cc1076
mallocLevy <berkercaner1@gmail.com>
2021-02-15 17:44:20 +0300

    oddoccurences.c editted

--- lesson2/OddOccurrencesInArray/OddOccurrences.c
+++ lesson2/OddOccurrencesInArray/OddOccurrences.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 
 int solution (int A[],int N){
 	int unpaired = A[0];	
@@ -9,8 +7,3 @@ int solution (int A[],int N){
 	return unpaired;
 }
 
-int main(){
-	int A[] = {13,1,5,1,3,4,2,5,4,2,6,3,6};
-	printf("\n%d\n",solution(A,13));
-	return 0;
-}
