596db1fb06b884c9a29b1724d824917ef509f86b
mallocLevy <berkercaner1@gmail.com>
2021-02-15 16:17:03 +0300

    oddoccurencess.c is editted

--- lesson2/OddOccurrencesInArray/OddOccurrences.c
+++ lesson2/OddOccurrencesInArray/OddOccurrences.c
@@ -3,8 +3,8 @@
 
 int solution (int A[],int N){
 	int unpaired = A[0];	
-	for(int i = 0; i < N; i++){
-		unpaired ^= A[0];	//taking xor of all elements will result the different one ==> 1^3^4^3^1 = 4^0^0 = 4
+	for(int i = 1; i < N; i++){
+		unpaired ^= A[i];	//taking xor of all elements will result the different one ==> 1^3^4^3^1 = 4^0^0 = 4
 	}
 	return unpaired;
 }
