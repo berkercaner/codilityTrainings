294a38edb9b489c041f338ef93c6097be7b710ef
mallocLevy <berkercaner1@gmail.com>
2021-02-16 14:38:07 +0300

    maxProductofthree.c is added

---
+++ lesson6/maxProductOfThree/maxProductOfThree.c
@@ -0,0 +1,15 @@
+int cmpFunc(const void* a, const void* b)
+	return (*(int*)a - *(int*)b);
+	
+int getMax(int v1, int v2)
+	return v1>=v2 ? v1 : v2;
+	
+int solution(int A[], int N){
+	int posProduct, mixProduct;
+	qsort(A,N,sizeof(int),cmpFunc);
+	
+	maxCase1 = A[N-1]*A[N-2]*A[N-3];
+	maxCase2 = A[0]*A[1]*A[N-1];
+	
+	return getMax(maxCase1, maxCase2);
+}
