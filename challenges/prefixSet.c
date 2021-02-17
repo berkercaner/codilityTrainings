a90f78ca632df4d10ec25dba53c0ad8660e64f7d
mallocLevy <berkercaner1@gmail.com>
2021-02-17 10:49:52 +0300

    challenges and prefixSet.c are added

---
+++ challenges/prefixSet.c
@@ -0,0 +1,11 @@
+int solution(int A[], int N){
+	int* checkA = calloc(N,sizeof(int));
+	int i, p = 0;
+	for(i=0; i<N; i++){
+		if(checkA[A[i]] == 0){
+			checkA[A[i]] = 1;
+			p = i;
+		}
+	}
+	return p;
+}
