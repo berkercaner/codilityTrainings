ebcc605d103fb559b34262e5642ffd8a9f5d629a
mallocLevy <berkercaner1@gmail.com>
2021-03-12 15:51:28 +0300

    brackets.c is added

---
+++ lesson7/brackets.c
@@ -0,0 +1,53 @@
+#include <string.h>
+
+int solution(char *S){
+/*
+	this solution has made without using stack operations
+*/
+	int openC = 0, openP = 0, openS =0;
+	int N = strlen(S);
+/*
+	openC = curly bracket open
+	openP = paranthesis opened
+	openS = square bracket opened
+*/
+	//condition from question
+	if(N == 0)
+		return 1;
+	//if element number isn't even number, than it's not a nested array
+	if(N%2 != 0)
+		return 0;
+	if(S[0] == ')' || S[0] == ']' || S[0] == '}')
+		return 0;
+	
+	for(int i=0; i<N; i++){
+		if(S[i] == '(')
+			openP++;
+		if(S[i] == '[')
+			openS++;
+		if(S[i] == '{')
+			openC++;
+		
+		if(S[i] == ')')	
+			if(S[i-1] == '[' || S[i-1] == '{' || openP == 0)
+				return 0;
+			else
+				openP--;
+		if(S[i] == ']')
+			if(S[i-1] == '(' || S[i-1] == '{' || openS == 0)
+				return 0;
+			else
+				openS--;
+		if(S[i] == '}')
+			if(S[i-1] == '[' || S[i-1] == '(' || openC == 0)
+				return 0;
+			else
+				openC--;
+	}	
+	
+	if(openP == 0 && openP == 0 && openS == 0)
+		return 1;
+	return 0;
+	
+	
+}
