252595df0e99dc72c2a98f1ef830e5ae133a353f
mallocLevy <berkercaner1@gmail.com>
2021-03-12 17:17:40 +0300

    brackets.py is added

--- lesson7/brackets.py
+++ lesson7/brackets.py
@@ -1,22 +1,28 @@
 def solution(S):
-
+	#condition from the question
 	if len(S) == 0:
 		return 1
 		
 	stack = []
+	#creating dictionary for comperison
 	pairDict = {'(': ')', '{': '}', '[': ']'}
 	
 	for c in S:
+		#creating array with opening brackets
 		if c == '(' or c == '[' or c == '{':
 			stack.append(c)
 		else:
+			#if there is nothing to pop when closing brackets occured, it's not a nested array
 			if len(stack) == 0:
 				return 0
+			#pop the last element
 			lastOpened = stack.pop()
+			#if it's not paired with closed bracket, it's not a nested array 
 			if pairDict[lastOpened] != c:
 				return 0
-				
+	#if there are still nonclosed brackets, it's not a nested array
 	if len(stack) != 0:
 		return 0
-		
+	
+	#everything fine if it end up here
 	return 1
