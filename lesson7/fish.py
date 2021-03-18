6d813738c1f5ef85db05ffecbb9c65edfd371300
mallocLevy <berkercaner1@gmail.com>
2021-03-18 10:31:31 +0300

    fish.py is added

---
+++ lesson7/fish.py
@@ -0,0 +1,15 @@
+def solution(A,B):
+	left = 0
+	stack = [] #fish stack for moving upwards
+	n  = len(A)
+	for i in range(n):
+		if B[i] == 1:
+			stack.append(A[i]
+		else:
+			if len(stack) > 0: #if there is a fish moving upwards
+				if stack[-1] < A[i]: #if size of moving downwards fish greater
+					stack.pop() #fish in the stack will be eaten
+			else:
+				left += 1
+	
+	return left + len(stack)
