b662a7db39079aa91f7e274bae20f863a43297ac
mallocLevy <berkercaner1@gmail.com>
2021-03-18 09:50:22 +0300

    dominator.py is added

---
+++ lesson8/dominator.py
@@ -0,0 +1,28 @@
+def getKey(val,dic):
+	for key, value in dic.items():
+		if value == val:
+			return key
+
+def solution(A):
+	if len(A) == 0:
+		return -1
+	elif len(A) == 1:
+		return 0
+	
+	stack = []
+	mydict = {}
+	occured = 0
+	N = len(A)
+	for i in range(N):
+		mydict[i] = A[i]
+	
+	A.sort()
+	holdLeader = A[N//2]
+	count = 0
+	for i in range(N):
+		if A[i] == holdLeader:
+			count += 1
+	if count > N//2:
+		index = getKey(holdLeader,mydict)
+		return index
+	return -1
